df5baccaa0ac2eafd4811361b2cc4b472e549546
Renaldy Prasetio <renireas@gmail.com>
2022-05-28 16:23:07 +0800

    Update empty state

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,15 +9,21 @@ use App\Datatables\CategoryDatatable;
 
 class FrontController extends Controller
 {
-
     public function index()
     {
         $menus = Menu::with('categories')->get()->groupBy('categories.*.slug')->all();
+        $uncategorized = Menu::doesntHave('categories')->get();
+        $categories = Category::all()->pluck('name', 'slug');
+        $featured = Menu::where('featured', 1)->get()->take(3);
+
+        if (!$uncategorized->isEmpty()) {
+            $menus['uncategorized'] = $uncategorized;
+            $categories['uncategorized'] = 'Others';
+        }
+
         foreach ($menus as $key => $menu) {
             $menus[$key] = array_chunk($menu->all(), ceil(count($menu) / 2));
         }
-        $categories = Category::all()->pluck('name', 'slug');
-        $featured = Menu::where('featured', 1)->get()->take(3);
 
         return view('index', compact('menus', 'categories', 'featured'));
     }
--- resources/views/front/featured.blade.php
+++ resources/views/front/featured.blade.php
@@ -11,40 +11,46 @@
             </div>
         </div>
         <div class="row">
-            <div class="fh5co-grid">
-                <div class="fh5co-v-half to-animate-2">
-                    <div class="fh5co-v-col-2 fh5co-bg-img"
-                        style="background-image: url('storage/images/{{ $featured[0]->image }}')">
-                    </div>
-                    <div class="fh5co-v-col-2 fh5co-text fh5co-special-1 arrow-left">
-                        <h2>{{ $featured[0]->name }}</h2>
-                        <span class="pricing">RM{{ $featured[0]->price }}</span>
-                        <p>
-                            {{ $featured[0]->description }}
-                        </p>
-                    </div>
-                </div>
-                <div class="fh5co-v-half">
-                    <div class="fh5co-h-row-2 to-animate-2">
+            @if (isset($featured[0]))
+                <div class="fh5co-grid">
+                    <div class="fh5co-v-half to-animate-2">
                         <div class="fh5co-v-col-2 fh5co-bg-img"
-                            style="background-image: url('storage/images/{{ $featured[1]->image }}')"></div>
-                        <div class="fh5co-v-col-2 fh5co-text arrow-left">
-                            <h2>{{ $featured[1]->name }}</h2>
-                            <span class="pricing">RM{{ $featured[1]->price }}</span>
-                            <p>{{ $featured[1]->description }}</p>
+                            style="background-image: url('storage/images/{{ $featured[0]->image }}')">
+                        </div>
+                        <div class="fh5co-v-col-2 fh5co-text fh5co-special-1 arrow-left">
+                            <h2>{{ $featured[0]->name }}</h2>
+                            <span class="pricing">RM{{ $featured[0]->price }}</span>
+                            <p>
+                                {{ $featured[0]->description }}
+                            </p>
                         </div>
                     </div>
-                    <div class="fh5co-h-row-2 fh5co-reversed to-animate-2">
-                        <div class="fh5co-v-col-2 fh5co-bg-img"
-                            style="background-image: url('storage/images/{{ $featured[2]->image }}')"></div>
-                        <div class="fh5co-v-col-2 fh5co-text arrow-right">
-                            <h2>{{ $featured[2]->name }}</h2>
-                            <span class="pricing">RM{{ $featured[2]->price }}</span>
-                            <p>{{ $featured[2]->description }}</p>
+                    <div class="fh5co-v-half">
+                        <div class="fh5co-h-row-2 to-animate-2">
+                            <div class="fh5co-v-col-2 fh5co-bg-img"
+                                style="background-image: url('storage/images/{{ $featured[1]->image }}')"></div>
+                            <div class="fh5co-v-col-2 fh5co-text arrow-left">
+                                <h2>{{ $featured[1]->name }}</h2>
+                                <span class="pricing">RM{{ $featured[1]->price }}</span>
+                                <p>{{ $featured[1]->description }}</p>
+                            </div>
+                        </div>
+                        <div class="fh5co-h-row-2 fh5co-reversed to-animate-2">
+                            <div class="fh5co-v-col-2 fh5co-bg-img"
+                                style="background-image: url('storage/images/{{ $featured[2]->image }}')"></div>
+                            <div class="fh5co-v-col-2 fh5co-text arrow-right">
+                                <h2>{{ $featured[2]->name }}</h2>
+                                <span class="pricing">RM{{ $featured[2]->price }}</span>
+                                <p>{{ $featured[2]->description }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @else
+                <div class="text-center">
+                    <h3>No featured menu</h3>
+                </div>
+            @endif
         </div>
     </div>
 </div>
